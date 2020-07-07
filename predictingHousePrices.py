f7776a3e11d4c27e3425a17a6ae414e9d14aa087
Fabian Karl <60648165+fabianKarl@users.noreply.github.com>
2020-07-07 15:31:35 +0200

    Update predictingHousePrices.py

--- predictingHousePrices.py
+++ predictingHousePrices.py
@@ -1,15 +1,3 @@
-# pip install virtualenv
-# virtualenv ml101
-# source ml101/bin/activate (windows: ml101\Scripts\activate)
-# pip install numpy
-# pip install pandas
-# pip install sklearn
-# pip install matplotlib
-# pip install tensorflow
-# pip install keras
-# jupyter notebook
-
-
 from sklearn.datasets import load_boston
 import pandas as pd
 import matplotlib.pyplot as plt
